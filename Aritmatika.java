04b19d483a2fd67bb2678876af459cfb4fa60785
unknown <septiawatimais@gmail.com>
2016-03-29 18:02:47 +0700

    tugasku

---
+++ Aritmatika.java
@@ -0,0 +1,18 @@
+public class Aritmatika {
+
+public static void main(String [] args) {
+	int a, b, c, d, e, f ;
+	a = 9;
+	b = 3;
+	c = a + b ;
+	d = a - b ;
+	e = a / b ;
+	f = a * b ; 
+
+	System.out.println("Hasilnya pertambahannya adalah :" + c);
+	System.out.println("Hasilnya pengurangannya adalah :" + d);
+	System.out.println("Hasilnya pembagiannya  adalah :" + e);
+	System.out.println("Hasilnya perkaliannya adalh :" + f);	
+
+}
+}
