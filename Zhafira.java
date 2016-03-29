9461e70d585a22b9bbee89b89da8ec4a11783f03
unknown <septiawatimais@gmail.com>
2016-03-29 18:28:09 +0700

    tugasku

---
+++ Zhafira.java
@@ -0,0 +1,21 @@
+public class Zhafira {
+
+public static void main (String[] args){
+
+Zhafira nama = new Zhafira();
+System.out.println (nama.namaSaya());
+nama.pertambahan();
+
+}
+
+public String namaSaya (){
+return "Wajadda";
+}
+
+public void pertambahan (){
+
+int a = 3 ;
+int b = 4 ;
+System.out.println(a+b);
+}
+}
