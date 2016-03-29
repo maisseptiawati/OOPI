e1a1d29914ca7f97c8d9167ee4dca0c05336de85
unknown <septiawatimais@gmail.com>
2016-03-29 18:40:37 +0700

    tugasku

---
+++ BelajarString.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belajarstring;
+
+/**
+ *
+ * @author Ma'is
+ */
+
+public class BelajarString {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    String x = "Hello World";
+    System.out.println (x.length());
+    System.out.println(x.equals("Hello World"));
+    System.out.println(x.isEmpty());
+    }
+    
+}
